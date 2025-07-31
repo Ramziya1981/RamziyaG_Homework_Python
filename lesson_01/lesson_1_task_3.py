8064ba9e50fc709514921c5105249b2ead7e39c9
Ramziya1981 <ramziya.giniyatullina@gmail.com>
2025-07-31 22:44:17 +0300

    Правка стрктуры каталогов

--- lesson_01/lesson_1_task_3.py
+++ lesson_01/lesson_1_task_3.py
@@ -1,3 +1,5 @@
-first_name = input("Введите ваше имя")
-last_name = input("Введите вашу фамилию:")
-print("Вас зовут:{first_name}{last_name}")
+first_name = input("Введите ваше имя: ")
+last_name = input("Введите вашу фамилию: ")
+print("Конкатенация>> Вас зовут: " + first_name + " " + last_name)
+print(f"Использование форматированной строки>> Вас зовут: {first_name}{last_name}")
+
---
+++ lesson_03/smartphone.py
@@ -0,0 +1,11 @@
+class Smartphone:
+    phone_brand = "Марка",
+    phone_model = "Модель",
+    phone_number = "Номер"
+    
+    def __init__(self, brand, model, phone_number):
+        self.brand = brand
+        self.model = model
+        self.number = phone_number
+
+  
