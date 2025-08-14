d660f34b5736c01a056a4eef7ed034dc8c8b3d24
Ramziya1981 <ramziya.giniyatullina@gmail.com>
2025-08-15 02:16:30 +0300

    Доработка по занятию №5

---
+++ lesson5/lesson05_task3.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+driver = webdriver.Firefox()
+driver.get("https://the-internet.herokuapp.com/inputs")
+
+
+text_field = driver.find_element(By.CSS_SELECTOR, "input")
+
+text_field.send_keys("100")
+text_field.clear()
+text_field.send_keys("200")
+
+driver.quit()
---
+++ lesson5/lesson05_task4.py
@@ -0,0 +1,18 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+options = webdriver.FirefoxOptions()
+driver = webdriver.Firefox(options=options)
+driver.get("http://the-internet.herokuapp.com/login")
+
+username =  driver.find_element(By.CSS_SELECTOR, "input#username")
+username.send_keys("tomsmith")
+password = driver.find_element(By.CSS_SELECTOR, "input#password")
+password.send_keys("SuperSecretPassword!")
+
+button = driver.find_element(By.CSS_SELECTOR, "button").click()
+flash = driver.find_element(By.CSS_SELECTOR, "div#flash")
+print(flash.text)
+
+driver.quit()
